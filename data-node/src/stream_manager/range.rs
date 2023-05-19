24de5d45afaa5202f5633ffd95f17997c2be4f40
Li Zhanhui <lizhanhui@gmail.com>
2023-05-19 02:42:17 +0000

    Add unit tests for StreamManager#Range
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- data-node/src/stream_manager/range.rs
+++ data-node/src/stream_manager/range.rs
@@ -15,6 +15,10 @@ impl Range {
         }
     }
 
+    pub(crate) fn committed(&self) -> Option<u64> {
+        self.committed
+    }
+
     pub(crate) fn commit(&mut self, offset: u64) {
         if let Some(ref mut committed) = self.committed {
             if offset <= *committed {
@@ -48,5 +52,57 @@ impl Range {
             Some(committed) => committed >= self.metadata.end().unwrap_or(self.metadata.start()),
             None => false,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use model::range::RangeMetadata;
+
+    #[test]
+    fn test_new() -> Result<(), Box<dyn Error>> {
+        let metadata = RangeMetadata::new(0, 0, 0, 0, None);
+        let range = super::Range::new(metadata);
+        assert_eq!(range.committed(), None);
+        assert!(!range.data_complete());
+        Ok(())
+    }
+
+    #[test]
+    fn test_commit() -> Result<(), Box<dyn Error>> {
+        let metadata = RangeMetadata::new(0, 0, 0, 0, None);
+        let mut range = super::Range::new(metadata);
+        range.commit(1);
+        assert_eq!(range.committed(), Some(1));
+
+        range.commit(0);
+        assert_eq!(range.committed(), Some(1));
+
+        range.commit(2);
+        assert_eq!(range.committed(), Some(2));
+        Ok(())
+    }
+
+    #[test]
+    fn test_seal() -> Result<(), Box<dyn Error>> {
+        let metadata = RangeMetadata::new(0, 0, 0, 0, None);
+        let mut range = super::Range::new(metadata.clone());
+        range.commit(1);
+
+        let mut metadata = RangeMetadata::new(0, 0, 0, 0, Some(1));
+        range.seal(&mut metadata);
+
+        assert_eq!(range.committed(), Some(1));
+        assert!(range.data_complete(), "Data should be complete");
+
+        let mut metadata = RangeMetadata::new(0, 0, 0, 0, Some(2));
+        range.seal(&mut metadata);
+
+        assert_eq!(range.committed(), Some(1));
+        assert_eq!(false, range.data_complete(), "Data should not be complete");
+
+        Ok(())
     }
 }
