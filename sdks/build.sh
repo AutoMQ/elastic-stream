23ce0e06d6ee06a3dcf7c71945853c800277c68f
Li Zhanhui <lizhanhui@gmail.com>
2023-05-25 17:37:36 +0800

    Update Java CI pipeline
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- sdks/build.sh
+++ sdks/build.sh
@@ -2,6 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR/.." || exit
+flatc --java -o 'sdks/sdk-java/frontend/src/main/generated' --java-package-prefix 'com.automq.elasticstream.client.flatc' --gen-object-api components/protocol/fbs/rpc.fbs components/protocol/fbs/model.fbs
 cargo build -p frontend 
 cp target/debug/libfrontend.so sdks/sdk-java/frontend/src/main/resources/META-INF/native/libfrontend.so
 cd sdks/sdk-java || exit
