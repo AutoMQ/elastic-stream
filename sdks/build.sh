e89d8d527a2d2eb6d15b3bdf417bdd771c579507
Li Zhanhui <lizhanhui@gmail.com>
2023-05-25 18:45:09 +0800

    No need to call flatc manually as build.rs already have it
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- sdks/build.sh
+++ sdks/build.sh
@@ -2,7 +2,6 @@
 
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR/.." || exit
-flatc --java -o 'sdks/sdk-java/frontend/src/main/generated' --java-package-prefix 'com.automq.elasticstream.client.flatc' --gen-object-api components/protocol/fbs/rpc.fbs components/protocol/fbs/model.fbs
 cargo build -p frontend -Z unstable-options --out-dir=sdks/sdk-java/frontend/src/main/resources/META-INF/native/
 cd sdks/sdk-java || exit
 mvn package
