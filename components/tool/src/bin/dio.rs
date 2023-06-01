af3f5d27a58640e51c69016e7b740bb243673d71
Li Zhanhui <lizhanhui@gmail.com>
2023-06-01 08:35:27 +0000

    Move dio to tool crate
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- components/tool/src/bin/dio.rs
+++ components/tool/src/bin/dio.rs
@@ -1,10 +1,7 @@
 use io_uring::{self, opcode, types, IoUring};
-use nix::fcntl;
 use std::{
     alloc::{self, Layout},
     error::Error,
-    fs::OpenOptions,
-    os::{fd::AsRawFd, unix::prelude::OpenOptionsExt},
 };
 
 const IO_DEPTH: u32 = 4096;
@@ -14,19 +11,45 @@ const FILE_SIZE: i64 = 10i64 * 1024 * 1024 * 1024;
 fn main() -> Result<(), Box<dyn Error>> {
     println!("PID: {}", std::process::id());
 
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .read(true)
-        .custom_flags(libc::O_DIRECT | libc::O_NONBLOCK)
-        .open("/data/data0")?;
+    let mut control_ring = io_uring::IoUring::builder().dontfork().build(32)?;
+    let file_path = "/data/data0";
+    let sqe = opcode::OpenAt::new(types::Fd(libc::AT_FDCWD), file_path.as_ptr())
+        .flags(libc::O_CREAT | libc::O_RDWR | libc::O_DIRECT | libc::O_DSYNC)
+        .mode(libc::S_IRWXU | libc::S_IRWXG)
+        .build()
+        .user_data(0);
+    unsafe { control_ring.submission().push(&sqe) }?;
+    control_ring.submit_and_wait(1)?;
+    let fd = {
+        let mut cq = control_ring.completion();
+        let cqe = cq.next().unwrap();
+        debug_assert_eq!(0, cqe.user_data(), "user-data is inconsistent");
+        cq.sync();
+        cqe.result()
+    };
+    if fd < 0 {
+        panic!("Failed to open {file_path}");
+    }
+    println!("Opened {file_path} with fd={fd}");
 
-    fcntl::fallocate(
-        file.as_raw_fd(),
-        fcntl::FallocateFlags::empty(),
-        0,
-        FILE_SIZE,
-    )?;
+    let sqe = opcode::Fallocate64::new(types::Fd(fd), FILE_SIZE)
+        .offset(0)
+        .mode(libc::FALLOC_FL_ZERO_RANGE)
+        .build()
+        .user_data(1);
+    unsafe { control_ring.submission().push(&sqe) }?;
+    control_ring.submit_and_wait(1)?;
+    {
+        let mut cq = control_ring.completion();
+        let cqe = cq.next().unwrap();
+        debug_assert_eq!(1, cqe.user_data(), "user-data is inconsistent");
+        cq.sync();
+        if cqe.result() >= 0 {
+            println!("Fallocate File[{file_path}, FD={fd}] to {} OK", FILE_SIZE);
+        } else {
+            panic!("Failed to fallocate, errno: {}", -cqe.result());
+        }
+    }
 
     let mut uring = IoUring::builder()
         .setup_iopoll()
@@ -51,7 +74,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let submitter = uring.submitter();
     submitter.register_buffers(&bufs)?;
-    submitter.register_files(&[file.as_raw_fd()])?;
+    submitter.register_files(&[fd])?;
     submitter.register_iowq_max_workers(&mut [2, 2])?;
 
     submitter.register_enable_rings()?;
