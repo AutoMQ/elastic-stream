1d6dc6fc9ef687c818f456dbc318a5facf2614e3
Li Zhanhui <lizhanhui@gmail.com>
2023-06-07 07:15:43 +0000

    Clippy check non-test code only for now
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- data-node/src/stream_manager/mod.rs
+++ data-node/src/stream_manager/mod.rs
@@ -1,7 +1,7 @@
 pub(crate) mod fetcher;
+pub(crate) mod manager;
 pub(crate) mod range;
 pub(crate) mod stream;
-pub(crate) mod manager;
 pub(crate) mod window;
 
 pub(crate) use manager::StreamManager;
