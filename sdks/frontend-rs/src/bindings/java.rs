f07cea4b99a987a7b73e77f6e3d2f1258eb804b1
Li Zhanhui <lizhanhui@gmail.com>
2023-06-07 08:22:28 +0000

    cargo fmt
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- sdks/frontend-rs/src/bindings/java.rs
+++ sdks/frontend-rs/src/bindings/java.rs
@@ -611,7 +611,7 @@ pub unsafe extern "system" fn Java_com_automq_elasticstream_client_jni_Stream_ap
             // # Safety
             // Java caller guarantees that `buf` will remain valid until `Future#complete` is called.
             // As a result, we can safely treat the slice as 'static.
-            let slice = unsafe {std::slice::from_raw_parts(buf, len)};
+            let slice = unsafe { slice::from_raw_parts(buf, len) };
             let buf = Bytes::from_static(slice);
             Ok(Command::Append {
                 stream,
