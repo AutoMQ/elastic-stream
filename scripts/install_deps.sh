7c13e710695b3f35c3a120e98abdb8899917e692
Li Zhanhui <lizhanhui@gmail.com>
2023-06-21 07:50:14 +0000

    Use prebuilt rocksdb binary if host arch is aarch64
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -25,7 +25,17 @@ try_install_flatc() {
 
 try_install_rocksdb() {
     if [ ! -f rocksdb.deb ]; then
-        wget https://github.com/lizhanhui/rocksdb/releases/download/rocksdb-v8.1.3/rocksdb.deb
+        arch=$(uname -m)
+        if [ "$arch" == "aarch64" ]; then
+            echo "Host arch is aarch64"
+            wget -O rocksdb.deb https://github.com/lizhanhui/rocksdb/releases/download/rocksdb-v8.1.3/rocksdb-aarch64.deb
+        elif [ "$arch" == "x86_64" ]; then
+            echo "Host arch is amd64"
+            wget -O rocksdb.deb https://github.com/lizhanhui/rocksdb/releases/download/rocksdb-v8.1.3/rocksdb.deb
+        else
+            echo "Unsupported arch"
+            exit 1
+        fi
     fi
     ./try_install.sh rocksdb.deb
     rm -f rocksdb.deb
@@ -35,4 +45,4 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit
 
 try_install_flatc
-# try_install_rocksdb
+try_install_rocksdb
