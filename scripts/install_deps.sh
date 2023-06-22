0233f5b33164b07fbe26acca8724ff4a6979a583
Li Zhanhui <lizhanhui@gmail.com>
2023-06-22 14:13:44 +0000

    Update rocksdb.deb
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -28,10 +28,10 @@ try_install_rocksdb() {
         arch=$(uname -m)
         if [ "$arch" == "aarch64" ]; then
             echo "Host arch is aarch64"
-            wget -O rocksdb.deb https://github.com/AutoMQ/rocksdb/releases/download/rocksdb-v8.1.1b1/rocksdb-aarch64.deb
+            wget -O rocksdb.deb https://github.com/AutoMQ/rocksdb/releases/download/rocksdb-v8.1.1b0/rocksdb-aarch64.deb
         elif [ "$arch" == "x86_64" ]; then
             echo "Host arch is amd64"
-            wget -O rocksdb.deb https://github.com/AutoMQ/rocksdb/releases/download/rocksdb-v8.1.1b1/rocksdb-amd64.deb
+            wget -O rocksdb.deb https://github.com/AutoMQ/rocksdb/releases/download/rocksdb-v8.1.1b0/rocksdb-amd64.deb
         else
             echo "Unsupported arch"
             exit 1
